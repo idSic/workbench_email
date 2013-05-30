b1dfd3e7e5fc3cd0eea70f2a48c251e98ded2af8
teknic <btate340@gmail.com>
2013-05-29 22:09:26 -0700

    drupal code standard updates

--- workbench_email.api.php
+++ workbench_email.api.php
@@ -17,15 +17,16 @@
  * to the form element during those ajax events. #kapooya.
  *
  * @param array $form
- *  The form array.
- * @param $email_transition
- *  The email transition object. Example, draft to needs review.
- * @param $user_groups
- *  An associative array of editors and users.
+ *   The form array.
+ * @param object $email_transition
+ *   The email transition object. Example, draft to needs review.
+ * @param array $user_groups
+ *   An associative array of editors and users.
  */
 function hook_workbench_email_create_form_element_alter(&$form, $email_transition, $user_groups) {
-  // $user_groups['editors'] => The workbench access editors. Could be an empty array.
-  // $user_groups['users'] => All the users available under the specified role (or author).
+  // $user_groups['editors'] => The workbench access editors. Could be empty.
+  // $user_groups['users'] => All users available under specified role. When
+  // the rid is 0, it represents the author. 
   if ($user_groups['editors'] && $email_transition->from_name == 'draft') {
     $form['example_element']['#default_value'] = '';
   }
