638d128c52f919c529b71a3d87132ce85d88c9b5
teknic <btate340@gmail.com>
2013-05-29 21:12:13 -0700

    applied code format from IDE

--- js/workbench_email.js
+++ js/workbench_email.js
@@ -9,7 +9,7 @@
     attach: function(context) {
       $('.workbench-email-notify-container').each(function(index) {
         var notify_cont = $(this);
-        notify_cont.find('.workbench-email-notify-checkbox').click( function() {
+        notify_cont.find('.workbench-email-notify-checkbox').click(function() {
           notify_cont.next('.workbench-email-auto-notify-container').toggle();
         });
       });
